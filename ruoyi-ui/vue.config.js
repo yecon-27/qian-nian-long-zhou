df573906bc7d9522dff9f062b56aca397a25a5b9
Ye Cong <1306248@wku.edu.cn>
2025-08-05 08:44:52 +0800

    ry_fe_network_export

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -11,7 +11,7 @@ const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
 
 const baseUrl = 'http://localhost:8080' // 后端接口
 
-const port = process.env.port || process.env.npm_config_port || 80 // 端口
+const port = process.env.port || process.env.npm_config_port || 3000 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
